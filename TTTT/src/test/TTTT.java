f21528d66e929318903577aab7bcc85580d4a8e8
HOME <HOME@DESKTOP-5SKJAR1>
2018-06-12 23:25:43 +0530

    ap

--- TTTT/src/test/TTTT.java
+++ TTTT/src/test/TTTT.java
@@ -5,6 +5,10 @@ public class TTTT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hii git hii hello hgoebgnwobnlkij;njl acdickjnkjLnSVNJi");
+System.out.println("hii git hii hello hgoebgnwobnlkij;njl acdickjnkjLnSVNJi");
+System.out.println("hii git hii hello hgoebgnwobnlkij;njl acdickjnkjLnSVNJi");
+System.out.println("hii git hii hello hgoebgnwobnlkij;njl acdickjnkjLnSVNJi");
+System.out.println("hii git hii hello hgoebgnwobnlkij;njl acdickjnkjLnSVNJi");
 	}
 
 }
